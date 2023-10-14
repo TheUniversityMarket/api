1d46db4e2d62af2223bbf3d170bd517c9afe9257
Paul Evans <84854052+pevans927@users.noreply.github.com>
2023-10-14 11:37:45 -0400

    Added sign-up request model

--- src/request_models/user_requests.py
+++ src/request_models/user_requests.py
@@ -3,3 +3,12 @@ from pydantic import BaseModel
 class SignInRequest(BaseModel):
     username: str
     password: str
+
+class SignUpRequest(BaseModel):
+    username: str
+    password: str
+    email: str
+    name: str
+    number: str
+    address: str
+    language: str
