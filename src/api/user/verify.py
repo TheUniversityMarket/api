6ac02b5b1a494f31657ea465cc2f644ac2072943
Paul Evans <84854052+pevans927@users.noreply.github.com>
2023-10-15 06:41:53 -0400

    Small final api checks

--- src/api/user/verify.py
+++ src/api/user/verify.py
@@ -3,12 +3,19 @@ from src.models.User.User import User
 from mailjet import send_message
 
 def get_verification_code(db, email, USERS_TO_VERIFY):
-    code = str(uuid4())[0:6]
-    USERS_TO_VERIFY.append({'email': email, 'code': code})
+    try:
+        if "@" not in email: 
+            return {'success':False,'error': "Invalid email"}
+        if "gatech" not in email:
+            return {'success':False,'error': "Must use a Georgia Tech email"}
+        code = str(uuid4())[0:6]
+        USERS_TO_VERIFY.append({'email': email, 'code': code})
 
-    # send email with code
-    send_message(email, f"Your verification code is {code}", "UMarket Verification Code")
-    return {'success':True,'error': None}
+        # send email with code
+        send_message(email, f"Your verification code is {code}", "UMarket Verification Code")
+        return {'success':True,'error': None}
+    except Exception as e:
+        return {'success':False,'error': str(e)}
 
 def check_verification_code(db, email, code, USERS_TO_VERIFY):
     for user in USERS_TO_VERIFY:
